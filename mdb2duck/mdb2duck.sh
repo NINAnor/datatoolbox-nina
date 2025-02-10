f87067b127508bb225d54e24afe6b0acd3f9cb2a
Niccolò Cantù <niccolo.cantu@nina.no>
2025-02-10 10:32:09 +0100

    remove nix shell shebang, use double quote

--- mdb2duck/mdb2duck.sh
+++ mdb2duck/mdb2duck.sh
@@ -1,21 +1,20 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p mdbtools duckdb parallel
+#! /usr/bin/bash
 
 set -e
 export FILENAME=$1
 
 process_table() {
-    export TABLENAME=$1
+    export TABLENAME=$(echo $1 | xargs)
     if [ -z "${TABLENAME}" ]; then
         return
     fi
-    echo $TABLENAME
-    mdb-json -U $FILENAME $TABLENAME > "${TABLENAME}.json"
-    duckdb "$FILENAME.duckdb" "$(envsubst<query.sql)"
+    echo "${TABLENAME}"
+    mdb-json -U "${FILENAME}" "${TABLENAME}" > "${TABLENAME}.json"
+    duckdb "${FILENAME}.duckdb" "$(envsubst<query.sql)"
     rm "${TABLENAME}.json"
     echo 'Inserted.'
 }
 
 export -f process_table
 
-mdb-tables -T $FILENAME | awk '{print $2" "$3}' | parallel -j1 process_table {1} ::::
+mdb-tables -T "${FILENAME}" | awk '{print $2" "$3}' | parallel -j1 process_table {1}
